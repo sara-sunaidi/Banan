b6758c228aabd1f1c51f0bc81c6f0fe471640c39
Madawi Ahmed <79159873+MadawiAh@users.noreply.github.com>
2022-03-23 19:28:46 +0300

    Pop up dialogs commit

---
+++ Banan/Controllers/CustomAcknowledgementViewController.swift
@@ -0,0 +1,80 @@
+//
+//  CustomAcknowledgementViewController.swift
+//  Banan
+//
+//  Created by Madawi Ahmed
+//
+import Foundation
+import UIKit
+import SwiftUI
+
+class CustomAcknowledgementViewController: UIView {
+
+    static let instance = CustomAcknowledgementViewController()
+    
+    @IBOutlet weak var parentView: UIView!
+    @IBOutlet weak var alertView: UIView!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var message: UILabel!
+    
+    // enum to perform diffrent format for each AcknowledgementType.
+    enum AcknowledgementType {
+        case positive
+        case negative
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        Bundle.main.loadNibNamed("CustomAcknowledgementView", owner: self, options: nil)
+        
+        commonInit()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commonInit() {
+        
+        Bundle.main.loadNibNamed("CustomAcknowledgementView", owner: self, options: nil)
+        
+        // img format
+        img.layer.cornerRadius = img.bounds.size.width/2
+        img.layer.borderColor = UIColor.white.cgColor
+        img.layer.borderWidth = 5
+        
+        // alert dialog format
+        alertView.layer.cornerRadius = 50
+        
+        parentView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        parentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+    func showAlert(title: String, message: String, acknowledgementType: AcknowledgementType) {
+        
+        commonInit()
+        
+        self.title?.text = title
+        self.message?.text = message
+        
+        switch acknowledgementType {
+            
+        case .positive:
+            img?.image = UIImage(named: "CheckSign")
+            
+        case .negative:
+            img?.image = UIImage(named: "ExclamationMark")
+        } // end switch
+        
+        UIApplication.shared.keyWindow?.addSubview(parentView!)
+    }
+    
+    @IBAction func onClickDone(_ sender: Any) {
+            parentView.removeFromSuperview()
+    }
+   
+    
+}
---
+++ Banan/Controllers/CustomConfirmationViewController.swift
@@ -0,0 +1,85 @@
+//
+//  CustomConfirmationViewController.swift
+//  Banan
+//
+//  Created by Madawi Ahmed
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+// Protocol in UIView Class for navigation purposes
+protocol CustomConfirmationViewControllerDelegate {
+    func didYesButtonTapped()
+}
+
+class CustomConfirmationViewController : UIView {
+    
+    static let instance = CustomConfirmationViewController()
+    
+    
+    @IBOutlet weak var parentView: UIView!
+    @IBOutlet weak var alertView: UIView!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var message: UILabel!
+    
+    var delegate: CustomConfirmationViewControllerDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        Bundle.main.loadNibNamed("CustomConfirmationView", owner: self, options: nil)
+        commonInit()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commonInit() {
+        
+        Bundle.main.loadNibNamed("CustomConfirmationView", owner: self, options: nil)
+        
+        // img format
+        img.layer.cornerRadius = img.bounds.size.width/2
+        img.layer.borderColor = UIColor.white.cgColor
+        img.layer.borderWidth = 5
+        
+        // alert dialog format
+        alertView.layer.cornerRadius = 50
+        
+        parentView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        parentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+    func showAlert(title: String, message: String) {
+        print("### in showAlert confirmation")
+        commonInit()
+        
+        self.title?.text = title
+        self.message?.text = message
+//        if we needed to change img :
+            img?.image = UIImage(named: "ExclamationMark")
+
+        UIApplication.shared.keyWindow?.addSubview(parentView!)
+    }
+    
+    @IBAction func onClickYes(_ sender: Any) {
+        print("### in Yes btn ")
+        parentView.removeFromSuperview()
+        
+        delegate?.didYesButtonTapped()
+        
+    }
+    
+    
+    @IBAction func onClickCancel(_ sender: Any) {
+        print("### in Cancel btn ")
+        parentView.removeFromSuperview()
+
+    }
+    
+    
+}
