ffa34a7781ad6db4d0051597653dfe171c3b3ac6
NSR116m <nourasaadr1@gmail.com>
2022-04-08 17:10:19 +0300

    add dashboard classes

---
+++ Banan/BoardRow.swift
@@ -0,0 +1,59 @@
+//
+//  BoardRow.swift
+//  Banan
+//
+//  Created by Noura  on 07/09/1443 AH.
+//
+
+import SwiftUI
+
+struct BoardRow: View {
+    
+    var b: Board
+    
+    var body: some View {
+        HStack{
+            Spacer()
+            VStack(alignment: .trailing, spacing: 8
+                   , content: {
+                Text(b.level)
+                    .foregroundColor(Color(red: 0.137, green: 0.263, blue: 0.271))
+                    .font(.custom("Almarai", size: 23))
+               
+                HStack(alignment: .bottom, spacing: 0, content:{
+                    
+                    Text("مجموع النقاط")
+                        .foregroundColor(Color(red: 0.525, green: 0.502, blue: 0.486))
+                        .font(.custom("Almarai", size: 18))
+                    Image("shinyStar2")
+                        .resizable()
+                        .frame(width: 23, height: 23)
+                })
+                
+                Text("بروقرس بار") // UIKit
+            })
+            
+            Image(b.getEvaluationInfo())
+                .resizable()
+                .padding()
+                .frame(width: 150, height: 150)
+                
+        }
+        .listRowBackground(Color.clear)
+//        .background(Color.clear)
+//        Text("Im in your VC HOORAAY !!")
+//            .padding()
+//            .foregroundColor(.red)
+//            .frame(width: 150, height: 150, alignment: .center)
+//            .background(Color.yellow)
+    }
+}
+
+struct BoardRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            BoardRow(b:Board(level: "المستوى الأول", points: 70, eval: .excellent))
+            BoardRow(b:Board(level: "المستوى الأول", points: 70, eval: .excellent))
+        }
+    }
+}
---
+++ Banan/Controllers/DashboardViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DashboardViewController.swift
+//  Banan
+//
+//  Created by Noura  on 07/09/1443 AH.
+//
+
+import UIKit
+import SwiftUI
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBSegueAction func uplaodList(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: BoardList())
+    }
+    
+    /*
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
