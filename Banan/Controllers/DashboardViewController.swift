c51e5f960da237203ab08b220633522136aae191
NSR116m <nourasaadr1@gmail.com>
2022-04-13 00:48:53 +0300

    Revert "fix error"
    This reverts commit b6f5e74170c201913e954df9dd8b5e7e16eb5269.

--- Banan/Controllers/DashboardViewController.swift
+++ Banan/Controllers/DashboardViewController.swift
@@ -27,14 +27,8 @@ class DashboardViewController: UIViewController {
         let allPoints = child.GameLevels.map({Int(($0["UserPoints"] ?? "0")) ?? 0}).reduce(0, +)
         self.totalPoints.text = "\(allPoints)".convertedDigitsToLocale(Locale(identifier: "AR"))
         
-        if(child.GameLevels.contains(["0": "0"])){
-            self.totalLevels.text = "0".convertedDigitsToLocale(Locale(identifier: "AR"))
-        }else{
-            let allLevels = child.GameLevels.map({ $0["Level"] ?? "0"})
-            self.totalLevels.text = "\(allLevels.count)".convertedDigitsToLocale(Locale(identifier: "AR"))
-        }
-        
-
+        let allLevels = child.GameLevels.map({ $0["Level"] ?? "0" })
+        self.totalLevels.text = "\(allLevels.count)".convertedDigitsToLocale(Locale(identifier: "AR"))
 
         
   
